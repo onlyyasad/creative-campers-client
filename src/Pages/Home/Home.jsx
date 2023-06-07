7f329d35ebc0684ddf544c057b02a61345f8056c
onlyyasad <onlyyasad@gmail.com>
2023-06-07 19:06:19 +0600

    Popular Instructors Section Initialized

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../Shared/Banner";
+import PopularClasses from "./PopularClasses";
+import PopularInstructors from "./PopularInstructors";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <PopularClasses></PopularClasses>
+            <PopularInstructors></PopularInstructors>
         </div>
     );
 };
---
+++ src/Pages/Home/PopularClasses.jsx
@@ -0,0 +1,79 @@
+
+
+const PopularClasses = () => {
+    return (
+        <div className="my-8">
+            <h1 className="uppercase font-bold text-3xl my-8 text-center">Popular Classes</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
+                {/* Card 1 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 2 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 3 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 4 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 5 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 6 */}
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://www.centauriartscamp.com/wp-content/uploads/IMG_0680-Copy-600x400.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Enroll Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PopularClasses;
