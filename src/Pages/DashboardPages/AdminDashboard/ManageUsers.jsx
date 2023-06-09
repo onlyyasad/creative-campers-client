f6e143839019bde543509ddd035f8a1b5a250759
onlyyasad <onlyyasad@gmail.com>
2023-06-09 19:41:44 +0600

    all users data fetch successfully done with axios secure

--- src/Pages/DashboardPages/AdminDashboard/ManageUsers.jsx
+++ src/Pages/DashboardPages/AdminDashboard/ManageUsers.jsx
@@ -1,9 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const ManageUsers = () => {
+
+    const [axiosSecure] = useAxiosSecure();
+    const { data: users = [], refetch } = useQuery(["users"], async () => {
+        const res = await axiosSecure.get("/users")
+        return res.data;
+    })
+
     return (
         <div>
-            This is manage users page
+            <h1>Total Users: {users.length}</h1>
+            {
+                users.map(user => <div className="flex gap-4" key={user._id}><h1>{user?.name}</h1><p>{user.email}</p><p>{user.role}</p></div>)
+            }
         </div>
     );
 };
